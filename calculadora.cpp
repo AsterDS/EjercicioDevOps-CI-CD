e3afaf853ce78c1533de5ac214b3a1c1ab7a344d
AsterDS <115512905+AsterDS@users.noreply.github.com>
2025-12-16 17:07:37 -0600

    Clculadora

---
+++ calculadora.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    char op;
+
+    cout << "=== Calculadora CI/CD C++ ===" << endl;
+    cout << "Ingresa una operacion (ej: 5 + 3): ";
+
+    cin >> a >> op >> b;
+
+    if (!cin) {
+        cerr << "Entrada invalida." << endl;
+        return 1;
+    }
+
+    switch (op) {
+        case '+':
+            cout << "Resultado: " << (a + b) << endl;
+            break;
+        case '-':
+            cout << "Resultado: " << (a - b) << endl;
+            break;
+        case '*':
+            cout << "Resultado: " << (a * b) << endl;
+            break;
+        case '/':
+            if (b == 0) {
+                cerr << "Error: Division entre cero." << endl;
+                return 1;
+            }
+            cout << "Resultado: " << (a / b) << endl;
+            break;
+        default:
+            cerr << "Operador no valido. Usa + - * /" << endl;
+            return 1;
+    }
+
+    return 0;
+}
